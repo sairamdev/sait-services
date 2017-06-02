e1ce0a6900fbc2518719e3d337ee2ccfe15d80d5
sairamdev <sai.empaty@gmail.com>
2017-06-02 15:31:04 +0530

    Ami-GO services

---
+++ sait-services/src/com/sait/amigo/user/AmigoUser.java
@@ -0,0 +1,15 @@
+package com.sait.amigo.user;
+import com.mongodb.*;
+
+public class AmigoUser {
+
+	public AmigoUser() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
