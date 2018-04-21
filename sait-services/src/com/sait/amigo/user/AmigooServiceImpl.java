83a55dede6b70bfa7301d3706230a2d7724a2ef3
sairamdev <sai.empaty@gmail.com>
2018-04-21 18:49:06 +0530

    Added the Logging mechanism and exception handling.

--- sait-services/src/com/sait/amigo/user/AmigooServiceImpl.java
+++ sait-services/src/com/sait/amigo/user/AmigooServiceImpl.java
@@ -4,7 +4,6 @@
 package com.sait.amigo.user;
 
 import java.util.ArrayList;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -12,13 +11,14 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.bson.Document;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.result.DeleteResult;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 /**
  * @author home
@@ -30,6 +30,8 @@ public class AmigooServiceImpl {
 	/**
 	 * 
 	 */
+	private static final Logger logger = LogManager.getLogger("AmigooServiceImpl");
+
 	public AmigooServiceImpl() {
 		// TODO Auto-generated constructor stub
 	}
@@ -38,42 +40,68 @@ public class AmigooServiceImpl {
 	@Path("setMyCarInfo")
 	@Consumes(MediaType.APPLICATION_JSON)
 	// @Produces(MediaType.APPLICATION_JSON)
-	public void setMyCarInfo(JsonNode inputCarData) {
+	public ArrayList<Document> setMyCarInfo(JsonNode inputCarData) {
+
 		/*
 		 * MyCar myCar = new MyCar(); return myCar;
 		 */
 		// return Object;
-		AmigooDBImpl amigoDbImpl = new AmigooDBImpl();
-		MongoClient mongoClient = amigoDbImpl.getMongoClient();
-		MongoDatabase database = mongoClient.getDatabase("amigoo");
-		MongoCollection<Document> myCarDetailColl = database.getCollection("mycar-details");
-		Document myCarDoc = Document.parse(inputCarData.toString());
-		myCarDetailColl.insertOne(myCarDoc);
+		ArrayList<Document> addCarResult = new ArrayList<Document>();
+		try {
+			AmigooDBImpl amigoDbImpl = new AmigooDBImpl();
+			MongoClient mongoClient = amigoDbImpl.getMongoClient();
+			MongoDatabase database = mongoClient.getDatabase("amigoo");
+			MongoCollection<Document> myCarDetailColl = database.getCollection("mycar-details");
+			Document myCarDoc = Document.parse(inputCarData.toString());
+			myCarDetailColl.insertOne(myCarDoc);
+			Document doc = new Document();
+			doc.append("AddCarResult", "Car Added Sucessfully");
+			addCarResult.add(doc);
 
+		} catch (Exception e) {
+			Document doc = new Document();
+			doc.append("Failed", e);
+			addCarResult.add(doc);
+			logger.error("Failed", e);
+			// TODO: handle exception
+		}
+		return addCarResult;
 	}
 
+	// logger.warn("Firstog Message");
 	@POST
 	@Path("getMyCarInfo")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public ArrayList<Document> getMyCarInfo(JsonNode inputCarData) {
-		String userId = inputCarData.get("UserID").asText();
-		AmigooDBImpl amigoDbImpl = new AmigooDBImpl();
-		MongoClient mongoClient = amigoDbImpl.getMongoClient();
-		MongoDatabase database = mongoClient.getDatabase("amigoo");
-		MongoCollection<Document> myCarDetailColl = database.getCollection("mycar-details");
-
-		// Document myDoc = myCarDetailColl.find(Filters.eq("UserID",
-		// "251615")).first();
-		FindIterable<Document> results = myCarDetailColl.find(Filters.eq("UserID", userId));
 		ArrayList<Document> myCarList = new ArrayList<Document>();
-		for (Document doc : results) {
-			System.out.println(doc);
+		try {
+			String userId = inputCarData.get("UserID").asText();
+			AmigooDBImpl amigoDbImpl = new AmigooDBImpl();
+			MongoClient mongoClient = amigoDbImpl.getMongoClient();
+			MongoDatabase database = mongoClient.getDatabase("amigoo");
+			MongoCollection<Document> myCarDetailColl = database.getCollection("mycar-details");
+
+			// Document myDoc = myCarDetailColl.find(Filters.eq("UserID",
+			// "251615")).first();
+			FindIterable<Document> results = myCarDetailColl.find(Filters.eq("UserID", userId));
+
+			for (Document doc : results) {
+				System.out.println(doc);
+				myCarList.add(doc);
+			}
+
+			return myCarList;
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("Failed", e);
+			Document doc = new Document();
+			doc.append("error", e);
 			myCarList.add(doc);
+			// TODO: handle exception
 		}
 
 		return myCarList;
-
 	}
 
 	@POST
@@ -81,12 +109,25 @@ public class AmigooServiceImpl {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public void setMyCarBrkDwnInfo(JsonNode inputCarBrkDown) {
-		AmigooDBImpl amigoDbImpl = new AmigooDBImpl();
-		MongoClient mongoClient = amigoDbImpl.getMongoClient();
-		MongoDatabase database = mongoClient.getDatabase("amigoo");
-		MongoCollection<Document> myCarBrkDownColl = database.getCollection("mycar-breakdown");
-		Document myCarBrkDownDoc = Document.parse(inputCarBrkDown.toString());
-		myCarBrkDownColl.insertOne(myCarBrkDownDoc);
+		ArrayList<Document> addBrkDwn = new ArrayList<Document>();
+		try {
+			AmigooDBImpl amigoDbImpl = new AmigooDBImpl();
+			MongoClient mongoClient = amigoDbImpl.getMongoClient();
+			MongoDatabase database = mongoClient.getDatabase("amigoo");
+			MongoCollection<Document> myCarBrkDownColl = database.getCollection("mycar-breakdown");
+			Document myCarBrkDownDoc = Document.parse(inputCarBrkDown.toString());
+			myCarBrkDownColl.insertOne(myCarBrkDownDoc);
+			Document doc = new Document();
+			doc.append("AddBreakDownResult", "Success");
+			addBrkDwn.add(doc);
+		} catch (Exception e) {
+			Document doc = new Document();
+			doc.append("Failed", e);
+			addBrkDwn.add(doc);
+			logger.error("Failed", e);
+			// TODO: handle exception
+		}
+
 	}
 
 	@POST
@@ -94,21 +135,31 @@ public class AmigooServiceImpl {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public ArrayList<Document> getMyCarBrkDwnInfo(JsonNode inputCarData) {
-		String userId = inputCarData.get("UserID").asText();
-		AmigooDBImpl amigoDbImpl = new AmigooDBImpl();
-		MongoClient mongoClient = amigoDbImpl.getMongoClient();
-		MongoDatabase database = mongoClient.getDatabase("amigoo");
-		MongoCollection<Document> myCarDetailColl = database.getCollection("mycar-breakdown");
-
-		// Document myDoc = myCarDetailColl.find(Filters.eq("UserID",
-		// "251615")).first();
-		FindIterable<Document> results = myCarDetailColl.find(Filters.eq("UserID", userId));
 		ArrayList<Document> myCarBreakDownList = new ArrayList<Document>();
-		for (Document doc : results) {
-			System.out.println(doc);
-			myCarBreakDownList.add(doc);
-		}
+		try {
+			String userId = inputCarData.get("UserID").asText();
+			AmigooDBImpl amigoDbImpl = new AmigooDBImpl();
+			MongoClient mongoClient = amigoDbImpl.getMongoClient();
+			MongoDatabase database = mongoClient.getDatabase("amigoo");
+			MongoCollection<Document> myCarDetailColl = database.getCollection("mycar-breakdown");
 
+			// Document myDoc = myCarDetailColl.find(Filters.eq("UserID",
+			// "251615")).first();
+			FindIterable<Document> results = myCarDetailColl.find(Filters.eq("UserID", userId));
+
+			for (Document doc : results) {
+				System.out.println(doc);
+				myCarBreakDownList.add(doc);
+			}
+
+		} catch (Exception e) {
+			Document doc = new Document();
+			doc.append("Failed", e);
+			myCarBreakDownList.add(doc);
+			logger.error("Failed", e);
+
+			// TODO: handle exception
+		}
 		return myCarBreakDownList;
 	}
 
@@ -117,21 +168,29 @@ public class AmigooServiceImpl {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public ArrayList<Document> deleteMyCarInfo(JsonNode inputCarData) {
-		
-		AmigooDBImpl amigoDbImpl = new AmigooDBImpl();
-		MongoClient mongoClient = amigoDbImpl.getMongoClient();
-		MongoDatabase database = mongoClient.getDatabase("amigoo");
-		MongoCollection<Document> myCarDetailColl = database.getCollection("mycar-details");
-
-		
-		String carId = inputCarData.get("carId").asText();
 		ArrayList<Document> deleteStatus = new ArrayList<Document>();
-		Document doc = new Document();
-		DeleteResult result = myCarDetailColl.deleteOne(Filters.eq("carId", carId));
-		doc.append("DeletedStatus", result);
-		doc.append("DeletedCarId", carId);
-		// doc.append("DelCount", result.getDeletedCount());
-		deleteStatus.add(doc);
+		try {
+			AmigooDBImpl amigoDbImpl = new AmigooDBImpl();
+			MongoClient mongoClient = amigoDbImpl.getMongoClient();
+			MongoDatabase database = mongoClient.getDatabase("amigoo");
+			MongoCollection<Document> myCarDetailColl = database.getCollection("mycar-details");
+
+			String carId = inputCarData.get("carId").asText();
+
+			Document doc = new Document();
+			DeleteResult result = myCarDetailColl.deleteOne(Filters.eq("carId", carId));
+			doc.append("DeletedStatus", result);
+			doc.append("DeletedCarId", carId);
+			// doc.append("DelCount", result.getDeletedCount());
+			deleteStatus.add(doc);
+
+		} catch (Exception e) {
+			Document doc = new Document();
+			doc.append("Failed", e);
+			deleteStatus.add(doc);
+			logger.error("Failed", e);
+
+		}
 		return deleteStatus;
 
 	}
@@ -141,21 +200,28 @@ public class AmigooServiceImpl {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public ArrayList<Document> deleteBrkDwnHistory(JsonNode inputCarData) {
-		String brkDwnId = inputCarData.get("brkDwnId").asText();
-		AmigooDBImpl amigoDbImpl = new AmigooDBImpl();
-		MongoClient mongoClient = amigoDbImpl.getMongoClient();
-		MongoDatabase database = mongoClient.getDatabase("amigoo");
-		MongoCollection<Document> myCarBrkDwnColl = database.getCollection("mycar-breakdown");
-
-		// Document myDoc = myCarDetailColl.find(Filters.eq("UserID",
-		// "251615")).first();
-		// String brkDwnId = inputCarData.get("brkDwnId").asText();
 		ArrayList<Document> deleteStatus = new ArrayList<Document>();
-		Document doc = new Document();
-		DeleteResult result = myCarBrkDwnColl.deleteOne(Filters.eq("brkDwnId", brkDwnId));
-		doc.append("DelStatus", result);
-		// doc.append("DelCount", result.getDeletedCount());
-		deleteStatus.add(doc);
+		try {
+			String brkDwnId = inputCarData.get("brkDwnId").asText();
+			AmigooDBImpl amigoDbImpl = new AmigooDBImpl();
+			MongoClient mongoClient = amigoDbImpl.getMongoClient();
+			MongoDatabase database = mongoClient.getDatabase("amigoo");
+			MongoCollection<Document> myCarBrkDwnColl = database.getCollection("mycar-breakdown");
+
+			// Document myDoc = myCarDetailColl.find(Filters.eq("UserID",
+			// "251615")).first();
+			// String brkDwnId = inputCarData.get("brkDwnId").asText();
+
+			Document doc = new Document();
+			DeleteResult result = myCarBrkDwnColl.deleteOne(Filters.eq("brkDwnId", brkDwnId));
+			doc.append("DelStatus", result);
+			// doc.append("DelCount", result.getDeletedCount());
+			deleteStatus.add(doc);
+
+		} catch (Exception e) {
+			logger.error("Failed", e);
+			// TODO: handle exception
+		}
 		return deleteStatus;
 	}
 
